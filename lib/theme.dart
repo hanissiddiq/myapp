f40629bf8de7b93ce2c90f3c8bff49224c71e18a
hanissiddiq10 <hanissiddiq10@gmail.com>
2024-08-19 13:56:16 +0000

    modified theme for apps

---
+++ lib/theme.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+
+Color primaryColor = Color(0xff55883B);
+Color secondaryColor = Color(0xffC1E899);
+Color alertColor = Color(0xff9A6735);
+Color primaryTextColor = Color(0xff000000);
+
+// TextStyle primaryTextStyle = GoogleFonts.poppins(
+//   color: primaryTextColor,
+// );
