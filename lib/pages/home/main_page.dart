65c5b96de55d209688c0816c9b43d5794753e3a6
hanissiddiq10 <hanissiddiq10@gmail.com>
2024-08-29 14:49:58 +0000

    add switch color and change page when tap Bottom Menu

--- lib/pages/home/main_page.dart
+++ lib/pages/home/main_page.dart
@@ -1,8 +1,19 @@
 import "package:flutter/material.dart";
+import "package:myapp/pages/home/cart_page.dart";
+import "package:myapp/pages/home/home_page.dart";
+import "package:myapp/pages/home/profile_page.dart";
+import "package:myapp/pages/home/wishlist_page.dart";
 import "package:myapp/theme.dart";
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -33,45 +44,72 @@ class MainPage extends StatelessWidget {
           clipBehavior: Clip.antiAlias,
           child: BottomNavigationBar(
             backgroundColor: primaryColor,
+            currentIndex: currentIndex,
+            onTap: (value) => setState(() {
+              print(value);
+              currentIndex = value;
+            }),
             type: BottomNavigationBarType.fixed,
             elevation: 0,
             items: [
               BottomNavigationBarItem(
-                icon: Image.asset(
-                  'assets/img/icon_home.png',
-                  width: 20,
+                icon: Container(
+                  margin: EdgeInsets.only(top: 0, bottom: 0),
+                  child: Image.asset(
+                    'assets/img/icon_home.png',
+                    width: 20,
+                    color:
+                        currentIndex == 0 ? backgroundColor5 : backgroundColor6,
+                  ),
                 ),
                 label: '',
                 tooltip: 'Home',
               ),
               BottomNavigationBarItem(
-                icon: Image.asset(
-                  'assets/img/icon_love.png',
-                  width: 20,
+                icon: Container(
+                  margin: EdgeInsets.only(top: 0, bottom: 0),
+                  child: Image.asset(
+                    'assets/img/icon_love.png',
+                    width: 20,
+                    color:
+                        currentIndex == 1 ? backgroundColor5 : backgroundColor6,
+                  ),
                 ),
                 label: '',
                 tooltip: 'Favorite',
               ),
               BottomNavigationBarItem(
-                icon: Image.asset(
-                  'assets/img/icon_cart.png',
-                  height: 4,
-                  width: 4,
+                icon: Container(
+                  margin: EdgeInsets.only(top: 0, bottom: 0),
+                  child: Image.asset(
+                    'assets/img/icon_cart.png',
+                    height: 4,
+                    width: 4,
+                  ),
                 ),
                 label: '',
               ),
               BottomNavigationBarItem(
-                icon: Image.asset(
-                  'assets/img/icon_cart.png',
-                  width: 20,
+                icon: Container(
+                  margin: EdgeInsets.only(top: 0, bottom: 0),
+                  child: Image.asset(
+                    'assets/img/icon_cart.png',
+                    width: 20,
+                    color:
+                        currentIndex == 3 ? backgroundColor5 : backgroundColor6,
+                  ),
                 ),
                 label: '',
                 tooltip: 'Cart',
               ),
               BottomNavigationBarItem(
-                icon: Image.asset(
-                  'assets/img/icon_profile.png',
-                  width: 20,
+                icon: Container(
+                  child: Image.asset(
+                    'assets/img/icon_profile.png',
+                    width: 20,
+                    color:
+                        currentIndex == 4 ? backgroundColor5 : backgroundColor6,
+                  ),
                 ),
                 label: '',
                 tooltip: 'Profile',
@@ -82,16 +120,31 @@ class MainPage extends StatelessWidget {
       );
     }
 
+    Widget body() {
+      switch (currentIndex) {
+        case 0:
+          return HomePage();
+          break;
+        case 1:
+          return WishlistPage();
+          break;
+        case 3:
+          return CartPage();
+          break;
+        case 4:
+          return ProfilePage();
+          break;
+        default:
+          return HomePage();
+      }
+    }
+
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 112, 14, 209),
       floatingActionButton: scanButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: custombottomNav(),
-      body: SafeArea(
-        child: Center(
-          child: Text('Main Page'),
-        ),
-      ),
+      body: body(),
     );
   }
 }
